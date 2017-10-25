52067d01e18049edb91c97283a4e8de307311bc0
Petar Slavic <petarslavic@gmail.com>
2017-10-25 15:26:33 +0100

    config and app changes

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
+    print os.environ['APP_SETTINGS']
     return "Hello World"
 
 @app.route('/<name>')
