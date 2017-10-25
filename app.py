d6836d669109de53bc48d08eddeb5bba9b4e0b72
Petar Slavic <petarslavic@gmail.com>
2017-10-25 15:48:22 +0100

    config out

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    print os.environ['APP_SETTINGS']
+    print(os.environ['APP_SETTINGS'])
     return "Hello World"
 
 @app.route('/<name>')
